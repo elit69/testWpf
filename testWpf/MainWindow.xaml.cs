a740d0f8072fef0d0e79701e209cdd7477d68986
elit <elit.chhorn@truemoney.com.kh>
2016-08-06 00:33:10 +0700

    draw ui

--- testWpf/MainWindow.xaml.cs
+++ testWpf/MainWindow.xaml.cs
@@ -24,5 +24,11 @@ namespace testWpf
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("yo");
+
+        }
     }
 }
